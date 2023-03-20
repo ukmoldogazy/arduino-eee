a51ec6e2e6f5bbc82b58d6e856bb85318a4517f4
MOLDOGAZY KABYLBEKOV <moldogazy.kabylbekov.kk@gmail.com>
2023-03-20 21:27:03 +0600

    Completed: Reading an SMS

--- step-motor/control-2-step-motors/control-2-step-motors.ino
+++ step-motor/control-2-step-motors/control-2-step-motors.ino
@@ -11,28 +11,28 @@ AccelStepper stepper1(HALFSTEP, 8, 10, 9, 11);
 AccelStepper stepper2(FULLSTEP, 4, 6, 5, 7);
 
 void setup() {
-	// set the maximum speed, acceleration factor,
-	// initial speed and the target position for motor 1
-	stepper1.setMaxSpeed(1000.0);
-	stepper1.setAcceleration(50.0);
-	stepper1.setSpeed(200);
-	stepper1.moveTo(2038);
+  // set the maximum speed, acceleration factor,
+  // initial speed and the target position for motor 1
+  stepper1.setMaxSpeed(1000.0);
+  stepper1.setAcceleration(50.0);
+  stepper1.setSpeed(200);
+  stepper1.moveTo(2038);
 
-	// set the same for motor 2
-	stepper2.setMaxSpeed(1000.0);
-	stepper2.setAcceleration(50.0);
-	stepper2.setSpeed(200);
-	stepper2.moveTo(-2038);
+  // set the same for motor 2
+  stepper2.setMaxSpeed(1000.0);
+  stepper2.setAcceleration(50.0);
+  stepper2.setSpeed(200);
+  stepper2.moveTo(-2038);
 }
 
 void loop() {
-	// Change direction once the motor reaches target position
-	if (stepper1.distanceToGo() == 0) 
-		stepper1.moveTo(-stepper1.currentPosition());
-	if (stepper2.distanceToGo() == 0) 
-		stepper2.moveTo(-stepper2.currentPosition());
+  // Change direction once the motor reaches target position
+  if (stepper1.distanceToGo() == 0)
+    stepper1.moveTo(-stepper1.currentPosition());
+  if (stepper2.distanceToGo() == 0)
+    stepper2.moveTo(-stepper2.currentPosition());
 
-	// Move the motor one step
-	stepper1.run();
-	stepper2.run();
+  // Move the motor one step
+  stepper1.run();
+  stepper2.run();
 }
